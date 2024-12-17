087f9b13045b12b2e3c610cfe0209b4f8e66a58d
GustavoWSa <76006075+GustavoWSa@users.noreply.github.com>
2024-12-17 21:16:25 +0000

    Atualizacoes 1
    Banco de dados setado
    Criado partes das dependencias para criar tela de adicionar alunos

---
+++ src/java/entidade/Aluno.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author gusta
+ */
+public class Aluno {
+
+    private int id;  
+    private String nome;
+    private String email;
+    private String celular;
+    private String cidade;
+    private String bairro;
+    private String cep;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    private String cpf; 
+    private String senha;
+    private String endereco;
+    
+    public Aluno (String nome, String cpf, String endereco, String senha, String cidade, String bairro, String cep, String email, String celular) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.senha = senha;
+        this.bairro = bairro;
+        this.celular = celular;
+        this.cidade = cidade;
+        this.email = email;
+        this.cep = cep;
+        
+    }
+
+    public Aluno(String cpf, String senha) {
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public Aluno() {
+        this.id = 0;
+        this.nome = "";
+        this.cpf = "";
+        this.endereco = "";
+        this.senha = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+
+    
+}
